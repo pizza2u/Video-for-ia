f79ba93c018e5fe1ceec909053cdefa67a17d0fd
Sara <55607380+pizza2u@users.noreply.github.com>
2024-04-26 16:29:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('playButton').addEventListener('click', function() {
+    var videoPlayer = document.getElementById('videoPlayer');
+    videoPlayer.style.display = 'block';
+});
